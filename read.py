f3eaed3663e6539957fb2aa7725b0514651026b4
Emily Hu <Smile0Hu55873@gmail.com>
2019-12-08 16:27:56 +0800

    add filter funtion

--- read.py
+++ read.py
@@ -11,5 +11,12 @@ print('檔案讀取完了,總共有', len(data), '筆資料')
 
 sum_len = 0
 for d in data:
-	sum_len = sum_len + len(d)
-print('平均每筆留言長度', sum_len/len(data))
+	sum_len = sum_len + len(d) #len(d)每一筆多少字
+print('平均每筆留言長度', sum_len/len(data)) #len(data)這份檔案有多少筆
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
+print(new[0])
